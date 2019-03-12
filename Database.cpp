2e58ea440278ac76f7241cd3752b008e5459684e
Liana <liana.mkhitaryan37@gmail.com>
2019-03-12 19:56:19 +0400

    Finish read function

--- Database.cpp
+++ Database.cpp
@@ -6,12 +6,12 @@ void Database::write(size_t key, double  elem)
 {
 	if (access_[std::this_thread::get_id()] == write_able) {
 		auto it = content_.find(key);
-		if (content_.find(key) == content_.end()) {
+		if (it == content_.end()) {
 			std::cout << elem << "inserted to db" << std::endl;
 			content_.insert({ key,elem });
 		}
 		else {
-			std::cout << elem << " elem with this key"<< key << "Updated in db" << std::endl;
+			std::cout << elem << " elem with this key "<< key << " Updated in db" << std::endl;
 			it->second = elem;
 		}
 		return;
@@ -21,5 +21,11 @@ void Database::write(size_t key, double  elem)
 
 void Database::read(size_t key)
 {
-	std::cout << "Read Called" <<  std::endl;
+	auto it = content_.find(key);
+	if (it == content_.end()) {
+		std::cout << "There is not such element in db"<< std::endl;
+	}
+	else {
+		std::cout << '{'<<key<<','<<it->second<<'}'<< std::endl;
+	}
 }
